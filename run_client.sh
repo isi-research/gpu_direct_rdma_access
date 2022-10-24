32565b0e659f799b183591a134fffe21666c475f
Pourya Shirazian <Pourya.Shirazian@intusurg.com>
2022-10-23 19:18:06 -0700

    scripts to run the client and server portions

---
+++ run_client.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "set the correct ip addresses on the server side pouryas-ud2"
+sudo ifconfig ibp97s0f0 up 192.168.0.200
+sudo ifconfig ibp97s0f1 up 192.168.0.210
+
+
+echo "running the server side"
+set -x
+./client -t 0 -a 192.168.0.200 192.168.0.100 -n 10000 -D 1 -s 3392160 -p 18001 &
+
+set +x
---
+++ run_server.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+echo "set the correct ip addresses on the server side pouryas-ud1"
+sudo ifconfig ibp15s0f0 up 192.168.0.100
+sudo ifconfig ibp15s0f1 up 192.168.0.110
+
+
+echo "running the server side"
+set -x
+./server -a 192.168.0.100 -n 10000 -D 1 -s 3392160 -p 18001 &
+
+set +x
